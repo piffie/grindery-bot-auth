93ad2ce23904d60d278b2685b626a62336f925bf
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-09-04 13:22:40 -0300

    db setup

---
+++ src/db/conn.js
@@ -0,0 +1,25 @@
+import {MongoClient} from "mongodb";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString);
+
+export class Database {
+  static instance;
+
+  static async getInstance(req) {
+    if (!Database.instance) {
+      let conn;
+      try {
+        conn = await client.connect();
+      } catch (e) {
+        console.error(e);
+      }
+
+      Database.instance = conn.db("grindery-bot");
+    }
+    return Database.instance;
+  }
+}
