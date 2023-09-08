163b7e6030202c5ee49d0df56d89214f358e307e
Thomas Coratger <thomas.coratger@gmail.com>
2023-09-08 11:49:26 +0200

    transfer formatting wip

--- src/routes/db.js
+++ src/routes/db.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { Database } from "../db/conn.js";
 import { authenticateApiKey } from "../utils/auth.js";
-import { getIncomingTxsUser, getOutcomingTxsUser } from "../utils/transfers.js";
+import {
+  getIncomingTxsUser,
+  getOutgoingTxsUser,
+  getRewardTxsUser,
+} from "../utils/transfers.js";
 
 const router = express.Router();
 
@@ -18,31 +22,17 @@ router.post("/:collectionName", authenticateApiKey, async (req, res) => {
   );
 });
 
-router.get("/incoming-transfers", authenticateApiKey, async (req, res) => {
+router.get("/format-transfers-user", authenticateApiKey, async (req, res) => {
   try {
-    return res
-      .status(200)
-      .send(
-        await getIncomingTxsUser(
-          await Database.getInstance(),
-          req.query.userTgId
-        )
-      );
-  } catch (error) {
-    return res.status(500).send({ msg: "An error occurred", error });
-  }
-});
+    const incomingTxs = await getIncomingTxsUser(
+      await Database.getInstance(),
+      req.query.userTgId
+    );
 
-router.get("/outcoming-transfers", authenticateApiKey, async (req, res) => {
-  try {
-    return res
-      .status(200)
-      .send(
-        await getOutcomingTxsUser(
-          await Database.getInstance(),
-          req.query.userTgId
-        )
-      );
+    const outgoingTxs = await getOutgoingTxsUser(
+      await Database.getInstance(),
+      req.query.userTgId
+    );
   } catch (error) {
     return res.status(500).send({ msg: "An error occurred", error });
   }
@@ -62,5 +52,77 @@ router.get("/:collectionName", authenticateApiKey, async (req, res) => {
     return res.status(500).send({ msg: "An error occurred", error });
   }
 });
+
+async function myFunction(userTgId) {
+  try {
+    const db = await Database.getInstance();
+
+    let finalText = "";
+
+    finalText += await getIncomingTxsUser(db, userTgId, 0, 0).then(
+      (incomingTxs) => {
+        return incomingTxs.length > 0
+          ? `<b>Incoming transfers:</b>\n${incomingTxs
+              .map(
+                (transfer) =>
+                  `${transfer.tokenAmount} g1 from @${
+                    transfer.senderUserHandle
+                  } on ${transfer.dateAdded} ${
+                    transfer.message
+                      ? `[text details: ${transfer.message}]`
+                      : ""
+                  }`
+              )
+              .join("\n")}\n\n`
+          : "";
+      }
+    );
+
+    finalText += await getOutgoingTxsUser(db, userTgId, 0, 0).then(
+      (outgoingTxs) => {
+        return outgoingTxs.length > 0
+          ? `<b>Outgoing transfers:</b>\n${outgoingTxs
+              .map(
+                (transfer) =>
+                  `${transfer.tokenAmount} g1 to @${
+                    transfer.recipientUserHandle
+                  } on ${transfer.dateAdded} ${
+                    transfer.message
+                      ? `[text details: ${transfer.message}]`
+                      : ""
+                  }`
+              )
+              .join("\n")}\n\n`
+          : "";
+      }
+    );
+
+    finalText += await getRewardTxsUser(db, userTgId, 0, 0).then(
+      (rewardTxs) => {
+        return rewardTxs.length > 0
+          ? `<b>Reward transfers:</b>\n${rewardTxs
+              .map(
+                (transfer) =>
+                  `${transfer.amount} g1 on ${transfer.dateAdded} ${
+                    transfer.message
+                      ? `[text details: ${transfer.message}]`
+                      : ""
+                  }`
+              )
+              .join("\n")}\n\n`
+          : "";
+      }
+    );
+
+    console.log(finalText.trimEnd());
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+    return ""; // En cas d'erreur, retourne une chaîne vide.
+  } finally {
+    process.exit(0);
+  }
+}
+
+myFunction("5695885361");
 
 export default router;
--- src/utils/transfers.js
+++ src/utils/transfers.js
@@ -1,11 +1,61 @@
 import { Database } from "../db/conn.js";
-import { TRANSFERS_COLLECTION } from "./constants.js";
+import {
+  REWARDS_COLLECTION,
+  TRANSFERS_COLLECTION,
+  USERS_COLLECTION,
+} from "./constants.js";
 
-export async function getIncomingTxsUser(db, userId) {
+export async function getIncomingTxsUser(db, userId, start, limit) {
+  return await Promise.all(
+    (
+      await db
+        .collection(TRANSFERS_COLLECTION)
+        .find({ recipientTgId: userId })
+        .skip(start)
+        .limit(limit)
+        .toArray()
+    ).map(async (entry) => ({
+      ...entry,
+      dateAdded: formatDate(entry.dateAdded),
+      senderUserHandle:
+        (
+          await db
+            .collection(USERS_COLLECTION)
+            .findOne({ userTelegramID: entry.senderTgId })
+        )?.userHandle || null,
+    }))
+  );
+}
+
+export async function getOutgoingTxsUser(db, userId, start, limit) {
+  return await Promise.all(
+    (
+      await db
+        .collection(TRANSFERS_COLLECTION)
+        .find({ senderTgId: userId })
+        .skip(start)
+        .limit(limit)
+        .toArray()
+    ).map(async (entry) => ({
+      ...entry,
+      dateAdded: formatDate(entry.dateAdded),
+      recipientUserHandle:
+        (
+          await db
+            .collection(USERS_COLLECTION)
+            .findOne({ userTelegramID: entry.recipientTgId })
+        )?.userHandle || null,
+    }))
+  );
+}
+
+export async function getRewardTxsUser(db, userId, start, limit) {
   return (
     await db
-      .collection(TRANSFERS_COLLECTION)
-      .find({ recipientTgId: userId })
+      .collection(REWARDS_COLLECTION)
+      .find({ userTelegramID: userId })
+      .skip(start)
+      .limit(limit)
       .toArray()
   ).map((entry) => ({
     ...entry,
@@ -13,26 +63,23 @@ export async function getIncomingTxsUser(db, userId) {
   }));
 }
 
-export async function getOutcomingTxsUser(db, userId) {
-  return (
-    await db
-      .collection(TRANSFERS_COLLECTION)
-      .find({ senderTgId: userId })
-      .toArray()
-  ).map((entry) => ({
-    ...entry,
-    dateAdded: formatDate(entry.dateAdded),
-  }));
-}
+// function formatDate(date) {
+//   return new Date(date).toLocaleString("en-US", {
+//     year: "numeric",
+//     month: "2-digit",
+//     day: "2-digit",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//     second: "2-digit",
+//     hour12: true,
+//     timeZoneName: "short",
+//   });
+// }
 
 function formatDate(date) {
-  return new Date(date).toLocaleString("en-US", {
-    year: "numeric",
-    month: "2-digit",
+  const options = {
     day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
-  });
+    month: "short",
+  };
+  return new Date(date).toLocaleDateString("en-US", options);
 }
