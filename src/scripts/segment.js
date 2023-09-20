6134cf27f46fe738957eaf02f3e23d3c7f0cb33b
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-09-20 12:34:05 -0300

    create segment import scripts

--- src/scripts/segment.js
+++ src/scripts/segment.js
@@ -1,36 +1,94 @@
-const axios = require("axios");
+import {Database} from "../db/conn.js";
+import axios from "axios";
 
 const SEGMENT_API_ENDPOINT = "https://api.segment.io/v1/batch";
-const SEGMENT_WRITE_KEY = "YOUR_SEGMENT_WRITE_KEY"; // Replace with your Segment write key
+const SEGMENT_WRITE_KEY = process.env.SEGMENT_WRITE_KEY;
 
-async function sendBatchRequest() {
+// Usage: sendUsersBatchRequest()
+// Description: Sends a batch request to the Segment API with user details.
+// This function fetches all users from the database, formats the user data according to the Segment API requirements,
+// and sends a batch request to the Segment API. The batch payload includes identification details,
+// user traits such as userName, userHandle, responsePath, and patchwallet, and the timestamp of when the user was added.
+// Example: sendUsersBatchRequest();
+async function sendUsersBatchRequest() {
   const db = await Database.getInstance();
   const usersCollection = db.collection("users");
 
   try {
-    // Fetch users from MongoDB
     const users = await usersCollection.find().toArray();
 
-    // Map users to the required format for the request payload
     const batch = users.map((user) => ({
       type: "identify",
-      userId: user._id.toString(), // Assuming `_id` is ObjectId type
+      userId: user.userTelegramID,
       traits: {
-        email: user.email,
-        name: user.name,
-        age: user.age,
+        userName: user.userName,
+        userHandle: user.userHandle,
+        responsePath: user.responsePath,
+        patchwallet: user.patchwallet,
       },
-      timestamp: new Date().toISOString(),
+      timestamp: user.dateAdded,
     }));
 
     const payload = {
       batch: batch,
-      context: {
-        device: {
-          type: "phone",
-          name: "Apple iPhone 6",
-        },
+    };
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${SEGMENT_WRITE_KEY}`,
+        "Content-Type": "application/json",
       },
+    };
+
+    const response = await axios.post(SEGMENT_API_ENDPOINT, payload, config);
+    console.log("Data sent successfully:", response.data);
+  } catch (error) {
+    console.log(error);
+    console.error(
+      "Error sending batch request:",
+      error.response ? error.response.data : error.message
+    );
+  } finally {
+    process.exit(0);
+  }
+}
+
+// Usage: sendTransfersBatchRequest()
+// Description: Sends a batch request to the Segment API with transfer details.
+// This function fetches all transfer records from the database, formats the transfer data according to the
+// Segment API requirements, and sends a batch request to the Segment API. The batch payload includes event details,
+// transaction properties like Amount, TxId, sender and recipient details, token details, and the timestamp of the transfer.
+// Example: sendTransfersBatchRequest();
+async function sendTransfersBatchRequest() {
+  const db = await Database.getInstance();
+  const transfersCollection = db.collection("transfers");
+
+  try {
+    const transfers = await transfersCollection.find().toArray();
+
+    const batch = transfers.map((transfer) => ({
+      type: "track",
+      userId: transfer.senderTgId,
+      event: "Transfer",
+      properties: {
+        Amount: transfer.tokenAmount,
+        TxId: transfer.TxId,
+        chainId: transfer.chainId,
+        recipientTgId: transfer.recipientTgId,
+        recipientWallet: transfer.recipientWallet,
+        senderName: transfer.senderName,
+        senderTgId: transfer.senderTgId,
+        senderWallet: transfer.senderWallet,
+        tokenAddress: transfer.tokenAddress,
+        tokenAmount: transfer.tokenAmount,
+        tokenSymbol: transfer.tokenSymbol,
+        transactionHash: transfer.transactionHash,
+      },
+      timestamp: transfer.dateAdded,
+    }));
+
+    const payload = {
+      batch: batch,
     };
 
     const config = {
@@ -51,5 +109,3 @@ async function sendBatchRequest() {
     process.exit(0);
   }
 }
-
-sendBatchRequest();
