b14af0034f1c569b811104b0d3f30de606acd463
Thomas Coratger <thomas.coratger@gmail.com>
2023-10-17 18:41:52 +0200

    add tests for error in PatchWallet transaction

--- src/test/pubsub-new-transaction.test.js
+++ src/test/pubsub-new-transaction.test.js
@@ -742,42 +742,88 @@ describe('handleNewTransaction function', async function () {
     chai.expect(result).to.be.true;
   });
 
-  it('Should return false and no transaction status modification in database if error in PatchWallet transaction', async function () {
-    axiosStub
-      .withArgs(patchwalletTxUrl)
-      .rejects(new Error('Service not available'));
+  describe('Error in PatchWallet transaction', async function () {
+    beforeEach(async function () {
+      axiosStub
+        .withArgs(patchwalletTxUrl)
+        .rejects(new Error('Service not available'));
 
-    await collectionUsersMock.insertOne({
-      userTelegramID: mockUserTelegramID,
-      userName: mockUserName,
-      userHandle: mockUserHandle,
-      patchwallet: mockWallet,
-      responsePath: mockResponsePath,
+      await collectionUsersMock.insertOne({
+        userTelegramID: mockUserTelegramID,
+        userName: mockUserName,
+        userHandle: mockUserHandle,
+        patchwallet: mockWallet,
+        responsePath: mockResponsePath,
+      });
     });
 
-    const result = await handleNewTransaction({
-      senderTgId: mockUserTelegramID,
-      amount: '100',
-      recipientTgId: mockUserTelegramID1,
-      eventId: txId,
+    it('Should return false if error in PatchWallet transaction', async function () {
+      const result = await handleNewTransaction({
+        senderTgId: mockUserTelegramID,
+        amount: '100',
+        recipientTgId: mockUserTelegramID1,
+        eventId: txId,
+      });
+
+      chai.expect(result).to.be.false;
     });
 
-    chai.expect(result).to.be.false;
-    chai
-      .expect(await collectionTransfersMock.find({}).toArray())
-      .excluding(['_id', 'dateAdded'])
-      .to.deep.equal([
-        {
-          eventId: txId,
-          chainId: 'eip155:137',
-          tokenSymbol: 'g1',
-          tokenAddress: process.env.G1_POLYGON_ADDRESS,
-          senderTgId: mockUserTelegramID,
-          recipientTgId: mockUserTelegramID1,
-          tokenAmount: '100',
-          status: TRANSACTION_STATUS.PENDING,
-        },
-      ]);
+    it('Should not modify database if error in PatchWallet transaction', async function () {
+      const result = await handleNewTransaction({
+        senderTgId: mockUserTelegramID,
+        amount: '100',
+        recipientTgId: mockUserTelegramID1,
+        eventId: txId,
+      });
+
+      chai
+        .expect(await collectionTransfersMock.find({}).toArray())
+        .excluding(['_id', 'dateAdded'])
+        .to.deep.equal([
+          {
+            eventId: txId,
+            chainId: 'eip155:137',
+            tokenSymbol: 'g1',
+            tokenAddress: process.env.G1_POLYGON_ADDRESS,
+            senderTgId: mockUserTelegramID,
+            recipientTgId: mockUserTelegramID1,
+            tokenAmount: '100',
+            status: TRANSACTION_STATUS.PENDING,
+          },
+        ]);
+    });
+
+    it('Should not call FlowXO if error in PatchWallet transaction', async function () {
+      await handleNewTransaction({
+        senderTgId: mockUserTelegramID,
+        amount: '100',
+        recipientTgId: mockUserTelegramID1,
+        eventId: txId,
+      });
+
+      chai.expect(
+        axiosStub
+          .getCalls()
+          .find(
+            (e) => e.firstArg === process.env.FLOWXO_NEW_TRANSACTION_WEBHOOK
+          )
+      ).to.be.undefined;
+    });
+
+    it('Should not call Segment if error in PatchWallet transaction', async function () {
+      await handleNewTransaction({
+        senderTgId: mockUserTelegramID,
+        amount: '100',
+        recipientTgId: mockUserTelegramID1,
+        eventId: txId,
+      });
+
+      chai.expect(
+        axiosStub
+          .getCalls()
+          .find((e) => e.firstArg === 'https://api.segment.io/v1/track')
+      ).to.be.undefined;
+    });
   });
 
   it('Should return true and transaction status to failure in database if error 470 in PatchWallet transaction', async function () {
