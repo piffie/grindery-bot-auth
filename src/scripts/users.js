525618728d38c730a733052f0fb330d9e53182b9
Thomas Coratger <thomas.coratger@gmail.com>
2023-11-01 20:48:08 +0100

    add new script to import new users

--- src/scripts/users.js
+++ src/scripts/users.js
@@ -2,6 +2,7 @@ import { Database } from '../db/conn.js';
 import fs from 'fs';
 import csv from 'csv-parser';
 import csvWriter from 'csv-writer';
+import { USERS_COLLECTION } from '../utils/constants.js';
 
 // Usage: importUsersFromCSV(filePath)
 // Description: This function imports user data from a CSV file into the database.
@@ -51,6 +52,75 @@ async function importUsersFromCSV(filePath) {
   } catch (error) {
     console.error('Error reading CSV file:', error);
     process.exit(1);
+  }
+}
+
+// Usage: importUsersFromJSON(filePath)
+// Description: This function imports user data from a JSON file into the database.
+// - filePath: The path to the JSON file containing user data.
+// Example: importUsersFromJSON("/path/to/your/file.json");
+async function importUsersFromJSON(filePath) {
+  try {
+    const db = await Database.getInstance();
+    const collection = db.collection(USERS_COLLECTION);
+    const existingUserIds = new Set(
+      (await collection.distinct('userTelegramID')).map(String)
+    );
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+
+    // Utilisation d'une expression régulière pour extraire les objets entre les accolades
+    const objectRegex = /{[^{}]*}/g;
+    const extractedObjects = fileContent.match(objectRegex);
+
+    const parsedObjects = [];
+    let malformedObjectCount = 0;
+
+    if (extractedObjects && extractedObjects.length > 0) {
+      for (const object of extractedObjects) {
+        try {
+          // Essayez de parser chaque objet JSON
+          const parsedObject = JSON.parse(object);
+
+          if (
+            parsedObject.user_id &&
+            parsedObject.user_id.trim() !== '' &&
+            parsedObject.bot_connection_id &&
+            parsedObject.bot_connection_id.trim() !== ''
+          ) {
+            // parsedObjects.push(parsedObject);
+
+            parsedObjects.push({
+              userTelegramID: parsedObject.user_id,
+              responsePath: `${parsedObject.bot_connection_id}/c/${parsedObject.user_id}`,
+              userHandle: parsedObject.user_handle,
+              userName: parsedObject.user_name,
+              patchwallet: '',
+              dateAdded: new Date(),
+            });
+          }
+        } catch (error) {
+          malformedObjectCount++;
+        }
+      }
+    }
+
+    const newUsers = parsedObjects.filter(
+      (newUser) => !existingUserIds.has(newUser.userTelegramID)
+    );
+
+    if (newUsers.length > 0) {
+      await collection.insertMany(newUsers);
+      console.log('New users inserted.');
+    } else {
+      console.log('No new users to insert.');
+    }
+
+    console.log(`Malformed objects count: ${malformedObjectCount}`);
+  } catch (error) {
+    console.error('Error importing users from JSON file:', error);
+  } finally {
+    process.exit(0);
   }
 }
 
