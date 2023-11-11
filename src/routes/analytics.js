1d77f965c74399eb2d0dcfd6c2d91c55a9e14ca5
Thomas Coratger <thomas.coratger@gmail.com>
2023-11-11 20:26:44 +0100

    add endpoint to get number txs last hour

---
+++ src/routes/analytics.js
@@ -0,0 +1,46 @@
+import Web3 from 'web3';
+import express from 'express';
+import 'dotenv/config';
+import ERC20 from './abi/ERC20.json' assert { type: 'json' };
+import BigNumber from 'bignumber.js';
+import { CHAIN_MAPPING } from '../utils/chains.js';
+import axios from 'axios';
+import { authenticateApiKey } from '../utils/auth.js';
+import { Database } from '../db/conn.js';
+import { TRANSFERS_COLLECTION } from '../utils/constants.js';
+
+const router = express.Router();
+
+/**
+ * GET /v1/transactions-last-hour
+ *
+ * @summary Get the number of transactions made in the last hour
+ * @description Retrieves the count of transactions made in the last hour.
+ * @tags Transactions
+ * @return {object} 200 - Number of transactions made in the last hour.
+ * @return {object} 500 - Error response.
+ * @example response - 200 - Success response example
+ * {
+ *   "count": 50
+ * }
+ * @example response - 500 - Error response example
+ * {
+ *   "msg": "An error occurred",
+ *   "error": "Server error description"
+ * }
+ */
+router.get('/transactions-last-hour', authenticateApiKey, async (req, res) => {
+  try {
+    const db = await Database.getInstance(req);
+
+    return res.status(200).json({
+      count: await db.collection(TRANSFERS_COLLECTION).countDocuments({
+        dateAdded: { $gte: new Date(Date.now() - 60 * 60 * 1000) },
+      }),
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: 'An error occurred', error });
+  }
+});
+
+export default router;
