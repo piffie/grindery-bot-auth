6215e9b6347f103dcd080f1fb48411f80da572db
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-12-07 22:53:16 +0100

    feat: add balance endpoint for users to get USD balance (#111)
    * add balance endpoint for users
    
    * add api key protection

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { Database } from '../db/conn';
 import { authenticateApiKey } from '../utils/auth';
 import { USERS_COLLECTION } from '../utils/constants';
+import axios from 'axios';
+import { ANKR_KEY } from '../../secrets';
 
 const router = express.Router();
 
@@ -72,6 +74,49 @@ router.get('/attributes', authenticateApiKey, async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).send({ msg: 'An error occurred', error });
+  }
+});
+
+router.get('/balance', authenticateApiKey, async (req, res) => {
+  console.log(`User [${req.query.userTelegramID}] requested balance`);
+  try {
+    const db = await Database.getInstance();
+
+    const user = await db
+      .collection(USERS_COLLECTION)
+      .findOne({ userTelegramID: req.query.userTelegramID });
+
+    if (!user) {
+      return res.status(400).json({ error: 'User is not in the database' });
+    }
+
+    const balance = await axios.post(
+      `https://rpc.ankr.com/multichain/${ANKR_KEY}`,
+      {
+        jsonrpc: '2.0',
+        method: 'ankr_getAccountBalance',
+        params: {
+          blockchain: req.query.chain
+            ? (req.query.chain as string).split(',')
+            : 'polygon',
+          walletAddress: user?.patchwallet || '',
+          onlyWhitelisted: false,
+        },
+        id: new Date().toString(),
+      },
+      {
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+
+    console.log(`User [${req.query.userTelegramID}] balance request completed`);
+    return res.status(200).json(balance.data?.result || {});
+  } catch (error) {
+    console.error(
+      `Error getting balance for user ${req.query.userTelegramID}`,
+      JSON.stringify(error),
+    );
+    return res.status(500).send({ success: false, error: 'An error occurred' });
   }
 });
 
