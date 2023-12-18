4244ad57fec802da27675e9003f644692d355766
Maurício Junior <junior9771@gmail.com>
2023-12-18 18:06:14 -0300

    remove scripts (#128)
    Co-authored-by: Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>

--- src/scripts/cronjob.ts
+++ src/scripts/cronjob.ts
@@ -1,30 +1,5 @@
 import cron from 'node-cron';
-import {
-  importUsersLast4Hours,
-  importTransfersLast4Hours,
-  importOrUpdateWalletUsersLast2Hours,
-} from './bigquery';
 import { distributeReferralRewards, distributeSignupRewards } from './rewards';
-
-// Schedule a task to run every hour
-cron.schedule('0 * * * *', async () => {
-  console.log('CRON - importUsersLast4Hours task');
-  try {
-    await importUsersLast4Hours();
-  } catch (error) {
-    console.log('CRON - importUsersLast4Hours error ', error);
-  }
-});
-
-// Schedule a task to run every hour
-cron.schedule('0 * * * *', async () => {
-  console.log('CRON - importTransfersLast4Hours task');
-  try {
-    await importTransfersLast4Hours();
-  } catch (error) {
-    console.log('CRON - importTransfersLast4Hours error ', error);
-  }
-});
 
 // Schedule a task at 00:00 on every day-of-month
 cron.schedule('0 0 */1 * *', async () => {
@@ -43,15 +18,5 @@ cron.schedule('0 0 */1 * *', async () => {
     distributeReferralRewards();
   } catch (error) {
     console.log('CRON - distributeReferralRewards error ', error);
-  }
-});
-
-// Schedule a task to run every hour
-cron.schedule('0 * * * *', async () => {
-  console.log('CRON - importOrUpdateWalletUsersLast2Hours task');
-  try {
-    importOrUpdateWalletUsersLast2Hours();
-  } catch (error) {
-    console.log('CRON - importOrUpdateWalletUsersLast2Hours error ', error);
   }
 });
