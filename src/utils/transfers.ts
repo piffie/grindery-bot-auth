13e4444989a2f561d8c826733bc4396fae599764
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-12-22 17:00:59 +0100

    move tx fmt types to dedicated file (#161)

--- src/utils/transfers.ts
+++ src/utils/transfers.ts
@@ -25,36 +25,10 @@ import {
   TransactionStatus,
 } from '../types/webhook.types';
 import { isPositiveFloat } from '../webhooks/utils';
-
-/**
- * Represents an Incoming Transaction from the database.
- */
-type IncomingTransaction = {
-  /**
-   * The date when the transaction was added.
-   */
-  dateAdded: string;
-
-  /**
-   * The handle of the sender user associated with the transaction.
-   */
-  senderUserHandle: string | null;
-
-  /**
-   * A message related to the transaction.
-   */
-  message: string;
-
-  /**
-   * The unique identifier of the transaction.
-   */
-  _id: ObjectId;
-
-  /**
-   * The amount of tokens associated with the transaction.
-   */
-  tokenAmount: string;
-};
+import {
+  IncomingTransaction,
+  OutgoingTransaction,
+} from '../types/tx_formatting.types';
 
 /**
  * Retrieves incoming transactions for a user from the database.
@@ -94,41 +68,6 @@ export async function getIncomingTxsUser(
     ),
   );
 }
-
-/**
- * Represents an Outgoing Transaction in the system.
- */
-type OutgoingTransaction = {
-  /**
-   * The unique identifier of the transaction.
-   */
-  _id: ObjectId;
-
-  /**
-   * The date when the transaction was added.
-   */
-  dateAdded: string;
-
-  /**
-   * The handle of the recipient user associated with the transaction, which can be a string or null.
-   */
-  recipientUserHandle: string | null;
-
-  /**
-   * The amount of tokens associated with the transaction.
-   */
-  tokenAmount: string;
-
-  /**
-   * The Telegram ID of the recipient user.
-   */
-  recipientTgId: string;
-
-  /**
-   * A message related to the transaction.
-   */
-  message: string;
-};
 
 /**
  * Retrieves outgoing transactions for a user from the database.
