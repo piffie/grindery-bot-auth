774484f09bd54fdb28b66ca0d07ba169a04f9685
Thomas Coratger <thomas.coratger@gmail.com>
2024-01-03 23:53:50 +0100

    small fix to display userTelegramID in tx log

--- src/webhooks/transaction.ts
+++ src/webhooks/transaction.ts
@@ -132,7 +132,7 @@ export async function handleNewTransaction(
     );
 
     console.log(
-      `[${transfer.txHash}] transaction from ${transfer.params.senderInformation?.senderTgId} to ${transfer.params.recipientTgId} for ${transfer.params.amount} with event ID ${transfer.params.eventId} finished.`,
+      `[${transfer.txHash}] transaction from ${transfer.params.senderInformation?.userTelegramID} to ${transfer.params.recipientTgId} for ${transfer.params.amount} with event ID ${transfer.params.eventId} finished.`,
     );
     return true;
   }
